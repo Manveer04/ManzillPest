a1d7ec4c39ade49f0952e5e877e19a2ddc5da0c7
Manveer04 <155811338+Manveer04@users.noreply.github.com>
2024-08-13 16:06:35 +0800

    Upload v1

---
+++ admin.js
@@ -0,0 +1,6 @@
+function forgotPassword() {
+  alert("Please contact your administrator for password reset for your employer login account.");
+}
+function signUp() {
+  alert("Please contact your administrator for an employer login account.");
+}
